30b3ac4b6a197f10f6750294b0596be3db95a48e
root <root@li1103-6.members.linode.com>
2020-07-05 21:02:22 +0000

    Working

--- app/run_app.py
+++ app/run_app.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 import os
 port_num = "5000"
 print(f"Disabling the firewall for port {port_num}.")
 os.system("sudo firewall-cmd --zone=public --add-port=5000/tcp --permanent")
 os.system("sudo firewall-cmd --reload")
 try:
-    os.system("python3 app.py")
+    os.system("uwsgi --master --https 0.0.0.0:5000,foobar.crt,foobar.key")
 except:
     pass
 finally:
